65d45adde3baa3eada77937f167cb47d5c4d27fa
rmamchyk <roman.mamchyk@gmail.com>
2020-01-14 12:12:31 +0200

    added styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import TextAreaHighlight from './components/TextareaHighlight';
 
-const App: React.FC = () => {
-  return (
-    <div>
-      <TextAreaHighlight />
-    </div>
-  );
+interface IState {
+  text: string;
+  regex: string;
+}
+
+class App extends Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      text: '',
+      regex: ''
+    }
+  }
+
+  onTextChange = (text: string) => {
+    this.setState({ text })
+  }
+
+  onRegexChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ regex: e.target.value });
+  }
+
+  createRegex = (regexString: string): [boolean, RegExp?] => {
+    try {
+      let regex: RegExp;
+      if (regexString.charAt(0) === '/') {
+        let parts = regexString.split('/');
+        parts.shift();
+        
+        var flags = parts.pop();
+        regexString = parts.join('/');
+        console.log(regexString)
+        regex = new RegExp(regexString, flags);
+      } else {
+        console.log(regexString)
+        regex = new RegExp(regexString, 'g');
+      }
+      return [true, regex];
+    } catch (error) {
+      console.log('Regex invalid')
+      return [false, undefined];
+    }
+  }
+
+  render() {
+    const { text, regex } = this.state;
+    const [isValid, highlight] = this.createRegex(regex);
+
+    return (
+      <div className="container">
+        <div className="form-group">
+          <div className="label-with-error">
+            <label>Test string</label>
+            {!isValid && <span>The regular expression is invalid</span>}
+          </div>
+          <TextAreaHighlight
+            value={text}
+            highlight={highlight}
+            onChange={this.onTextChange}
+          />
+        </div>
+  
+        <div className="form-group">
+          <label>Regular expression</label>
+          <input type="text" value={regex} onChange={this.onRegexChange} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/TextareaHighlight.tsx
+++ src/components/TextareaHighlight.tsx
@@ -1,34 +1,18 @@
 import React, { Component } from 'react';
 import './TextareaHighlight.scss';
 
-interface IState {
+interface IProps {
     value: string;
-    perspective: boolean;
-    highlightedText: string;
+    highlight?: RegExp;
+    onChange: (value: string) => void;
 }
 
-const DEFAULT_TEXT = `This demo shows how to highlight bits of text within a textarea. Alright, that's a lie. You can't actually render markup inside a textarea. However, you can fake it by carefully positioning a div behind the textarea and adding your highlight markup there. JavaScript takes care of syncing the content and scroll position from the textarea to the div, so everything lines up nicely. Hit the toggle button to peek behind the curtain. And feel free to edit this text. All capitalized words will be highlighted.`;
-
-class TextAreaHighlight extends Component<{}, IState> {
+class TextAreaHighlight extends Component<IProps> {
     private backdropRef = React.createRef<HTMLDivElement>();
     private textareaRef = React.createRef<HTMLTextAreaElement>();
 
-    constructor(props: {}) {
-        super(props);
-
-        this.state = {
-            perspective: false,
-            value: DEFAULT_TEXT,
-            highlightedText: this.applyHighlights(DEFAULT_TEXT)
-        }
-    }
-
     onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        const text = e.target.value;
-        this.setState({
-            value: text,
-            highlightedText: this.applyHighlights(text)
-        });
+        this.props.onChange(e.target.value);
     }
 
     onScroll = (event: any) => {
@@ -38,27 +22,24 @@ class TextAreaHighlight extends Component<{}, IState> {
         }
     }
 
-    applyHighlights(text: string) {
+    applyHighlights(text: string, regex: RegExp) {
         text = text
             .replace(/\n$/g, '\n\n')
-            .replace(/[A-Z].*?\b/g, '<mark>$&</mark>');
+            .replace(regex, '<mark>$&</mark>');
         return text;
     }
 
-    onTogglePerspective = () => {
-        this.setState(state => ({ perspective: !state.perspective}));
-    }
-
     render() {
-        const { value, highlightedText, perspective } = this.state;
+        const { value, highlight } = this.props;
+        const highlightedValue = highlight ? this.applyHighlights(value, highlight) : '';
 
         return (
-            <>
-                <div className={`container ${perspective ? 'perspective' : ''}`}>
+            <div className="textarea-highlight">
+                <div className="container">
                     <div className="backdrop" ref={this.backdropRef}>
                         <div
                             className="highlights"
-                            dangerouslySetInnerHTML={{ __html: highlightedText }}
+                            dangerouslySetInnerHTML={{ __html: highlightedValue }}
                         ></div>
                     </div>
 
@@ -70,8 +51,7 @@ class TextAreaHighlight extends Component<{}, IState> {
                         onScroll={this.onScroll}
                     ></textarea>
                 </div>
-                <button onClick={this.onTogglePerspective}>Toggle Perspective</button>
-            </>
+            </div>
         )
     }
 }
